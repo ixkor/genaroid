a10a0050dafd4a9fcff1b7b944fcd84fe75574c9
Alexey Skoryatin <alexey@skoryatin.net>
2015-09-11 00:24:36 +0300

    Added license into missed java files

--- compiler/src/main/java/net/xkor/genaroid/processing/SubProcessor.java
+++ compiler/src/main/java/net/xkor/genaroid/processing/SubProcessor.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package net.xkor.genaroid.processing;
 
 import net.xkor.genaroid.GenaroidEnvironment;
--- compiler/src/main/java/net/xkor/genaroid/wrap/BaseClassWrapper.java
+++ compiler/src/main/java/net/xkor/genaroid/wrap/BaseClassWrapper.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2015 Aleksei Skoriatin
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed To in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.xkor.genaroid.wrap;
 
 import com.sun.tools.javac.code.Symbol;
@@ -8,16 +24,15 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 public class BaseClassWrapper {
-    private final JavacElements utils;
-    private final String classFullName;
-    private final Symbol.ClassSymbol classSymbol;
-    private final HashMap<String, Symbol> membersCache = new HashMap<>();
-
     private static final Filter<Symbol> noFilter = new Filter<Symbol>() {
         public boolean accepts(Symbol var1) {
             return true;
         }
     };
+    private final JavacElements utils;
+    private final String classFullName;
+    private final Symbol.ClassSymbol classSymbol;
+    private final HashMap<String, Symbol> membersCache = new HashMap<>();
 
     public BaseClassWrapper(JavacElements utils, String classFullName) {
         this.utils = utils;
