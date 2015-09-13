dbaf2665020d840cb1b3a82e79bbc4d1b08d4345
Alexey Skoryatin <alexey@skoryatin.net>
2015-09-13 14:37:46 +0300

    Code cleaning

--- example/src/main/java/net/xkor/genaroid/example/MainActivity.java
+++ example/src/main/java/net/xkor/genaroid/example/MainActivity.java
@@ -18,15 +18,10 @@ package net.xkor.genaroid.example;
 
 import android.accounts.Account;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import net.xkor.genaroid.annotations.BaseActivity;
-import net.xkor.genaroid.annotations.BaseFragment;
 import net.xkor.genaroid.annotations.InstanceState;
 import net.xkor.genaroid.annotations.ViewById;
 
@@ -77,38 +72,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @BaseFragment
-    public static class TestFragment extends Fragment {
-
-        @ViewById(R.id.testId)
-        private View test;
-
-        @ViewById(R.id.testId)
-        private View test2;
-
-        @InstanceState
-        private String stringField;
-
-        @InstanceState
-        private int[] intArrayField;
-
-        @InstanceState
-        private float floatField;
-
-        @InstanceState
-        private Bundle bundleField;
-
-        @InstanceState
-        private ArrayList<String> stringArrayField;
-
-        @Nullable
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.activity_main, container, false);
-        }
-    }
-
-    public static class TestClass implements Serializable {
-        private int qwerty;
+    private static class TestClass implements Serializable {
     }
 }
---
+++ example/src/main/java/net/xkor/genaroid/example/TestFragment.java
@@ -0,0 +1,44 @@
+package net.xkor.genaroid.example;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.xkor.genaroid.annotations.BaseFragment;
+import net.xkor.genaroid.annotations.InstanceState;
+import net.xkor.genaroid.annotations.ViewById;
+
+import java.util.ArrayList;
+
+@BaseFragment
+public class TestFragment extends Fragment {
+    @ViewById(R.id.testId)
+    private View test;
+
+    @ViewById(R.id.testId)
+    private View test2;
+
+    @InstanceState
+    private String stringField;
+
+    @InstanceState
+    private int[] intArrayField;
+
+    @InstanceState
+    private float floatField;
+
+    @InstanceState
+    private Bundle bundleField;
+
+    @InstanceState
+    private ArrayList<String> stringArrayField;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_main, container, false);
+    }
+}
